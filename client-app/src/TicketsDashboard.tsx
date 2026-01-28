988b119b49b0c1776b30a278c3f94a93cb1bce0d
Ahmad Kaviani <ahmad.kaviani.86@gmail.com>
2026-01-28 15:50:06 +0330

    some little fixes

--- client-app/src/TicketsDashboard.tsx
+++ client-app/src/TicketsDashboard.tsx
@@ -6,37 +6,50 @@ import TicketDetail from './TicketDetail';
 import TicketForm from './TicketForm';
 import { useStore } from './stores/store';
 import { observer } from 'mobx-react-lite';
+import { useNavigate } from "react-router-dom";
 
 
 
 export default observer(function TicketsDashboard() {
 
     const { ticketStore } = useStore();
+    const navigate = useNavigate();
 
 
     return (
         <>
-            <Button positive content='ثبت تیکت جدید' onClick={() => { ticketStore.formOpen() }}></Button>
-            <Container style={{ marginTop: '2em' }} >
+            <Container style={{ marginTop: '2em' }}>
                 <Grid>
                     <Grid.Column width={13}>
+                        <fieldset style={{ border: "1px solid #ccc", padding: "1rem", borderRadius: "8px" }}>
+                            <legend style={{ fontSize: "1.2rem", fontWeight: "bold", padding: "0 8px" }}>
+                                تیکت های ثبت شده
+                            </legend>
 
-                        <TicketList ></TicketList>
+                            <TicketList />
 
+                            {ticketStore.selectedTicket && <TicketDetail />}
+                            {ticketStore.editMode && <TicketForm />}
+                        </fieldset>
                     </Grid.Column>
                     <Grid.Column width={3}>
-                        {ticketStore.selectedTicket &&
-                            <>
-                                <TicketDetail />
-                            </>
-                        }
-                        {ticketStore.editMode &&
-                            <TicketForm ></TicketForm>
-                        }
-
+                        <Button style={{ marginTop: '1em' }}
+                            positive
+                            fluid
+                            content="تیکت جدید"
+                            onClick={() => ticketStore.formOpen()}
+                        />
+                        <br />
+                        <Button
+                            negative
+                            fluid
+                            content="سوالات متداول"
+                            onClick={() => navigate("/faq")}
+                        />
                     </Grid.Column>
                 </Grid>
             </Container>
+
         </>
     );
 })
